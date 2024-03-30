44f8fc8cb45e2bcbffb38f19ba8f10991eda0b71
Joao Paulo Duprat Pohlmann <joaopaulo@aprovatotal.com.br>
2024-03-30 17:07:27 -0300

    Testing htmx with Go

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,61 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"text/template"
 )
 
-type Templates struct {
-	templates *template.Template
+type Contact struct {
+	Name string
+	Email string
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func newData() Data {
+	return Data {
+		Contacts: []Contact{
+			newContact("John", "jd@gmail.com"),
+			newContact("Clara", "cd@gmail.com"),
+		},
+	}
+}
+
+func newContact(name, email string) Contact {
+	return Contact{
+		Name: name,
+		Email: email,
+	}
+}
+
+var data = newData()
+
+func handleCount (w http.ResponseWriter, r *http.Request) {
+
+	block := "index"
+
+	if (r.Method == "POST") {
+		name := r.FormValue("name")
+		email := r.FormValue("email")
+
+		data.Contacts = append(data.Contacts, newContact(name,email))
+
+		block = "display"
+	}
+
+	tmpl := template.Must(template.ParseFiles("../views/index.html"))
+
+	tmpl.ExecuteTemplate(w, block, data)
 }
 
 func main() {
-	http.ListenAndServe(":8080", nil)
+
+	http.HandleFunc("/", handleCount)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
