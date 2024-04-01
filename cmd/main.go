10c38aa359bf3bfa6404ce6fa012d0d28106e0df
Joao Paulo Duprat Pohlmann <duprat.dev@gmail.com>
2024-03-31 22:35:56 -0300

    Adicionado out of band no html e refatorado web server

--- cmd/main.go
+++ cmd/main.go
@@ -7,55 +7,99 @@ import (
 )
 
 type Contact struct {
-	Name string
-	Email string
+    Name string
+    Email string
+}
+
+func newContact(name, email string) Contact {
+    return Contact{
+        Name: name,
+        Email: email,
+    }
 }
 
 type Contacts = []Contact
 
 type Data struct {
-	Contacts Contacts
+    Contacts Contacts
+}
+
+func (d *Data) hasEmail(email string) bool {
+    for _, contact := range d.Contacts {
+        if contact.Email == email {
+            return true
+        }
+    }
+    return false
 }
 
 func newData() Data {
-	return Data {
-		Contacts: []Contact{
-			newContact("John", "jd@gmail.com"),
-			newContact("Clara", "cd@gmail.com"),
-		},
-	}
+    return Data{
+        Contacts: []Contact{
+            newContact("John", "aoeu"),
+            newContact("Clara", "cd@gmail.com"),
+        },
+    }
 }
 
-func newContact(name, email string) Contact {
-	return Contact{
-		Name: name,
-		Email: email,
-	}
+type FormData struct {
+    Values map[string]string
+    Errors map[string]string
 }
 
-var data = newData()
+func newFormData() FormData {
+    return FormData{
+        Values: make(map[string]string),
+        Errors: make(map[string]string),
+    }
+}
 
-func handleCount (w http.ResponseWriter, r *http.Request) {
+type Page struct {
+    Data Data
+    Form FormData
+}
 
-	block := "index"
+func newPage() Page {
+    return Page{
+        Data: newData(),
+        Form: newFormData(),
+    }
+}
 
-	if (r.Method == "POST") {
-		name := r.FormValue("name")
-		email := r.FormValue("email")
 
-		data.Contacts = append(data.Contacts, newContact(name,email))
+var page = newPage()
 
-		block = "display"
-	}
+func handleContact(w http.ResponseWriter, r *http.Request) {
 
 	tmpl := template.Must(template.ParseFiles("../views/index.html"))
 
-	tmpl.ExecuteTemplate(w, block, data)
+	if (r.Method == "POST") {
+		name := r.FormValue("name")
+        email := r.FormValue("email")
+
+        if page.Data.hasEmail(email) {
+            formData := newFormData()
+            formData.Values["name"] = name
+            formData.Values["email"] = email
+            formData.Errors["email"] = "Email already exists"
+
+			w.WriteHeader(422)
+			tmpl.ExecuteTemplate(w, "form", formData)
+        } else {
+			contact := newContact(name, email)
+			page.Data.Contacts = append(page.Data.Contacts, contact)
+	
+			tmpl.ExecuteTemplate(w, "form", newFormData())
+			tmpl.ExecuteTemplate(w, "oob-contact", contact)
+		}
+	} else {
+		tmpl.ExecuteTemplate(w, "index", page)
+	}
 }
 
 func main() {
 
-	http.HandleFunc("/", handleCount)
+	http.HandleFunc("/", handleContact)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
